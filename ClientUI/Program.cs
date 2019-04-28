14c90c1670beeab233b1fae925e3898289d92f0c
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-04-28 16:20:27 -0500

    Sending Command

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using NServiceBus;
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -6,6 +8,8 @@ namespace ClientUI
 {
     class Program
     {
+        private static ILog log = LogManager.GetLogger<Program>();
+        
         static async Task Main(string[] args)
         {
             Console.Title = "ClientUI";
@@ -15,10 +19,40 @@ namespace ClientUI
 
             var endpointInstance = await Endpoint.Start(endpointConfiguration);
 
-            Console.WriteLine("Press enter to exit...");
-            Console.ReadLine();
+            await RunLoop(endpointInstance);
 
             await endpointInstance.Stop();
+        }
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                        await endpointInstance.SendLocal(command)
+                            .ConfigureAwait(false);
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        log.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
